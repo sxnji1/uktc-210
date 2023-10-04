840f6802498c83bc20088678883e79133074ad1c
dikel <dikelito@tutamail.com>
2023-10-04 23:12:29 +0300

    prepare file for exercise 2

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    private static final String NAME = "";
+    private static final int AGE = 0;
+    private static final String CITY = "";
+    private static final String FAV_MOVIE = "";
+
+    public static void main(String[] args) {
+        System.out.println("Hello, my name is " + NAME);
+        System.out.println("I'm " + AGE + " years old");
+        System.out.println("DELETE ME");
+        System.out.println("I live in " + CITY);
+        System.out.println("My favorite movie is " + FAV_MOVIE);
+    }
+}
